1ea4f40235fbda1a131f735add17a8eba1ec184a
sahil <sahil@LAPTOP-F4KDSST1>
2022-07-18 21:44:37 +0530

    fcgn

---
+++ Project1/Src/myStore/Test2.java
@@ -0,0 +1,23 @@
+package myStore;
+
+import java.util.Scanner;
+
+public class Test2 {
+void mathchallenge(int num1,int num2) {
+	if(num2>num1) {
+		System.out.println("true");
+	}
+	else {
+		System.out.println("-1");
+	}
+}
+	public static void main(String[] args) {
+		
+		Scanner s= new Scanner(System.in);
+		int num1=s.nextInt();
+		int num2 =s.nextInt();
+		Test2 obj = new Test2();
+		obj.mathchallenge(num1, num2);
+	}
+
+}
